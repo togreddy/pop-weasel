98f14d6c77e74b97a20d4e2a69a10bd799b7e719
togreddy <toareddy@gmail.com>
2015-10-20 17:23:02 -0500

    This is my first commit to the GitHub

---
+++ Practice/src/practice/java/edu/OddNumbers.java
@@ -0,0 +1,16 @@
+package practice.java.edu;
+
+public class OddNumbers {
+	
+	
+		public static void main(String org[])
+		{
+		int i;
+		for(i=1;i<=40;i++){//loop for take 1 to 100 numbers
+		if(i%2==1)//condition to find odd numbers between 1 to 100//(i%2!=0)
+		{
+		System.out.println(i);// print odd values between 1 to 40
+		}
+		}
+		}
+		}

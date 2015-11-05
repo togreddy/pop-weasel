4df3f90af352646784c6bc8ec29e26c0b02aeb38
togreddy <toareddy@gmail.com>
2015-11-05 16:16:53 -0600

    This is 2nd example to add and commit the project

---
+++ Javamodule1/src/CozaLoza.java
@@ -0,0 +1,43 @@
+
+public class CozaLoza {
+	
+	
+	
+	public static void main(String args[]){
+		
+		
+		for(int i=1;i<=110;i++)
+		{
+			
+			if(i%3==0)
+			{
+				System.out.print("coza ");
+			}
+			else if(i%5==0)
+			{
+				System.out.print("Loza ");
+			}
+			else if(i%7==0)
+			{
+				System.out.print("Woza ");
+			}
+			else if(i%11==0)
+			{
+				System.out.println(i);
+			}
+			else
+			{
+				System.out.print(i+ " ");
+			}
+			
+			
+			
+			
+			
+		}
+		
+	}
+	
+	
+
+}
